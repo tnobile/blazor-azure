9e049850e141907427296d901558092f7616b5af
tnobile <takao.nosaka@outlook.com>
2021-04-11 18:16:47 +0200

    covid faq

---
+++ Shared/Faq.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp.Shared
+{
+     public class Faq
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
